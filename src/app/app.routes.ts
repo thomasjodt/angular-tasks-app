292c0ca7c4f50f569e35dc5ef399a219ba5b919c
thomasjodt <jodt5200@gmail.com>
2024-08-08 00:28:53 -0500

    feat: Add the routes with lazy loading and the guard to tasks component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router'
+import { authGuardGuard } from './guards/auth-guard.guard'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/tasks'
+  },
+  {
+    path: 'tasks',
+    loadComponent: () => import('./todo/todo.component').then(comp => comp.TodoComponent),
+    canActivate: [authGuardGuard]
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(comp => comp.LoginComponent)
+  }
+]
