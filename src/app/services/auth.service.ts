e441ca86b3cad52608fe090c888a8dee2a1495d6
thomasjodt <jodt5200@gmail.com>
2024-08-08 00:23:25 -0500

    feat: Create a service that stores the credentials and have methods to work with them.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core'
+import { Observable, of } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  username: string = 'test01'
+  password: string = 'test01'
+
+  getAuth(): Observable<boolean> {
+    const storedToken = localStorage.getItem('authToken')
+    if (storedToken === null) return of(false)
+
+    const token = JSON.parse(storedToken)
+    return of(token.username === this.username && token.password === this.password)
+  }
+
+  setToken (username: string, password: string): void {
+    if (username !== this.username || password !== this.password) {
+      throw new Error('El usuario y/o contraseña son incorrectos')
+    }
+    const token = { username, password }
+    localStorage.setItem('authToken', JSON.stringify(token))
+  }
+
+  deleteToken (): void {
+    localStorage.removeItem('authToken')
+  }
+}
