8604798cf7419f36c2c93fe4bd902c971028f6c3
thomasjodt <jodt5200@gmail.com>
2024-08-08 00:25:48 -0500

    feat: Create the navbar thar shows the username and a button to log out.

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core'
+import { Router } from '@angular/router'
+import { AuthService } from '../../services/auth.service'
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+  auth = inject(AuthService)
+  router = inject(Router)
+  username!: string
+
+  logout(): void {
+    this.auth.deleteToken()
+    this.router.navigateByUrl('/login')
+  }
+
+  ngOnInit() {
+    const storedToken = localStorage.getItem('authToken')
+    if (storedToken !== null) {
+      const token = JSON.parse(storedToken)
+      this.username = token.username
+    }
+  }
+}
