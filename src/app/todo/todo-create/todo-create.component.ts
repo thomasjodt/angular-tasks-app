0f5fb333842618fb48ba851363aad2c6dd6ead30
thomasjodt <jodt5200@gmail.com>
2024-08-08 00:27:14 -0500

    feat: Create the component that will create the tasks.

---
+++ src/app/todo/todo-create/todo-create.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit, WritableSignal } from '@angular/core'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Todo } from '../../models/Todo'
+
+@Component({
+  selector: 'app-todo-create',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './todo-create.component.html'
+})
+export class TodoCreateComponent implements OnInit {
+  public createForm!: FormGroup
+  
+  @Input() todoList!: WritableSignal<Todo[]>
+
+  constructor (private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm = this.formBuilder.group({
+      newTodo: [
+        '',
+        Validators.required,
+      ]
+    })
+  }
+
+  public handleAddTodo () {
+    const value = this.createForm.value.newTodo.trim()
+    if (value === '') return
+
+    this.todoList.update(t => ([
+      ...t,
+      new Todo(value)
+    ]))
+  
+    this.createForm.reset()
+
+  }
+}
