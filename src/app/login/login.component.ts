ecbfd99a34cf3f554e8261a9d0d71015d8304893
thomasjodt <jodt5200@gmail.com>
2024-08-08 00:27:45 -0500

    feat: Create the login component.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common'
+import { Component, inject } from '@angular/core'
+import { Router } from '@angular/router'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { AuthService } from '../services/auth.service'
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  router = inject(Router)
+  auth = inject(AuthService)
+
+  loginForm!: FormGroup
+
+  constructor (private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.auth.getAuth().subscribe((isLogged) => {
+      if (isLogged) {
+        this.router.navigateByUrl('/tasks')
+        return
+      }
+    })
+
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login() {
+    try {
+      this.auth.setToken(this.loginForm.value.username, this.loginForm.value.password)
+      this.router.navigateByUrl('/tasks')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
