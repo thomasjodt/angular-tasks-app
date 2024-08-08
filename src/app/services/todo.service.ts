589b983c0d820a62e4812e9da0ee60b3fb2e5ee7
thomasjodt <jodt5200@gmail.com>
2024-08-08 00:24:05 -0500

    feat: Create a service to store and update the todos.

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal, effect } from '@angular/core'
+import { Todo } from '../models/Todo'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  public todoList = signal<Todo[]>([])
+
+  constructor() {
+    const storedTodos = localStorage.getItem('todos')
+    const todos: Todo[] = (storedTodos !== null) ? JSON.parse(storedTodos) : []
+
+    for (const todo of todos) {
+      this.todoList.update(t => ([
+        ...t,
+        new Todo(todo.description, todo.isDone, todo.id)
+      ]))
+    }
+
+    effect(() => {
+      localStorage.setItem(
+        'todos',
+        JSON.stringify(this.todoList())
+      )
+    })
+  }
+}
