f332364995ffb6ffbccda81d11b80f4f05a6ca3c
thomasjodt <jodt5200@gmail.com>
2024-08-18 00:01:36 -0500

    refactor: Change the username and password to an array of users, hydrate the store when the service initializes, add a method to find a user in the users array, change the implementation of isAuthenticated method, add a method to get the token in localStorage.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,61 @@
-import { Injectable } from '@angular/core'
-import { Observable, of } from 'rxjs'
+import { inject, Injectable } from '@angular/core'
+
+import { User } from '@/core/models'
+import { AppState, appStore, login } from '@/store'
+
+const TOKEN_KEY = 'authToken'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  username: string = 'test01'
-  password: string = 'test01'
+  private users: User[] = [
+    { username: 'test01', password: 'test01' }
+  ]
 
-  getAuth(): boolean {
-    const storedToken = localStorage.getItem('authToken')
+  private store = inject(appStore)
+
+  constructor () {
+    if (this.isAuthenticated()) {
+      const token = this.getToken()
+
+      this.store.dispatch(login({ token: {
+        username: token.username,
+        password: token.password
+      }}))
+    }
+  }
+
+  private findUser (username: string, password: string): User | null {
+    const user = this.users.find((user) => user.username === username)
+    if (user === undefined) return null
+    return (password === user.password) ? user : null
+  }
+
+  public isAuthenticated(): boolean {
+    const storedToken = localStorage.getItem(TOKEN_KEY)
     if (storedToken === null) return false
 
-    const token = JSON.parse(storedToken)
-    return token.username === this.username && token.password === this.password
+    const token: User = JSON.parse(storedToken)
+    
+    return this.findUser(token.username, token.password) !== null
   }
 
-  setToken (username: string, password: string): void {
-    if (username !== this.username || password !== this.password) {
+  public getToken (): User {
+    const storedToken = localStorage.getItem(TOKEN_KEY) as string
+    const token: User = JSON.parse(storedToken)
+    return token
+  }
+
+  public setToken (username: string, password: string): void {
+    if (this.findUser(username, password) === null) {
       throw new Error('El usuario y/o contraseña son incorrectos')
     }
-    const token = { username, password }
-    localStorage.setItem('authToken', JSON.stringify(token))
+    const token: User = { username, password }
+    localStorage.setItem(TOKEN_KEY, JSON.stringify(token))
   }
 
-  deleteToken (): void {
-    localStorage.removeItem('authToken')
+  public deleteToken (): void {
+    localStorage.removeItem(TOKEN_KEY)
   }
 }
