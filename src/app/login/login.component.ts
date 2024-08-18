b7b098781bff1f9ba4a2b3e034e358937f763f9a
thomasjodt <jodt5200@gmail.com>
2024-08-18 00:08:13 -0500

    feat: Dispatch the login action when press the login button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common'
 import { Component, inject, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
-import { AuthService } from '../services'
+
+import { AuthService } from '@/services'
+import { appStore, login } from '@/store'
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,8 @@ export class LoginComponent implements OnInit {
   private router = inject(Router)
   private auth = inject(AuthService)
   private formBuilder = inject(FormBuilder)
+
+  private store = inject(appStore)
 
   public loginForm!: FormGroup
   public errorMessage: string | null = null
@@ -28,6 +32,8 @@ export class LoginComponent implements OnInit {
   public login(): void {
     try {
       this.auth.setToken(this.loginForm.value.username, this.loginForm.value.password)
+
+      this.store.dispatch(login({ token: { username: this.loginForm.value.username, password: this.loginForm.value.password }}))
       this.router.navigateByUrl('/tasks')
     } catch (error) {
       const message = (error as Error).message
