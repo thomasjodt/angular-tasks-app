85b7f9e05207217fc62ad7c92ecaab2d19ce39c2
thomasjodt <jodt5200@gmail.com>
2024-10-26 11:20:16 -0500

    feat: Enable hash routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core'
-import { provideRouter } from '@angular/router'
+import { provideRouter, withHashLocation } from '@angular/router'
 
 import { routes } from './app.routes'
 import { provideStore } from '@ngrx/store'
 import { provideStoreDevtools } from '@ngrx/store-devtools'
-import { ROOT_REDUCERS } from './store/app.state'
+import { ROOT_REDUCERS } from './store'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideStore(ROOT_REDUCERS),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
